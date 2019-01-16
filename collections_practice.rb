80e3809f53c62b7b9651c85aa17864ea6be5d16c
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-16 20:13:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort {|a,b|  b<=>a }
+end
+def sort_array_char_count(array)
+  str=array.sort_by {|a| a.length  }
 
+end
+def swap_elements(array)
+array
+end
